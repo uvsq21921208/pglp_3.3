1e8ebaf571779a828f62c6b58ccb1116da580bc7
uvsq21921208 <mouttiedje@outlook.com>
2020-02-26 11:38:02 +0100

    Maven Creation

---
+++ src/main/java/fr/uvsq21921208/pglp_3_3/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq21921208.pglp_3_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
